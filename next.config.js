ca620403e40d73adba721ac81d610b769b7d56d1
Lazaro Placeres <lrodriguezplaceres@gmail.com>
2024-01-05 19:04:23 -0500

    sin pwa

--- next.config.js
+++ next.config.js
@@ -6,20 +6,21 @@ const nextConfig = {
       bodySizeLimit: "2mb",
     },
   },
-  /*env: {
+  env: {
     NEXT_PUBLIC_MI_API_BACKEND: "http://127.0.0.1:8000",
 
     NEXTAUTH_URL: "http://localhost:3000",
     //openssl rand -base64 32
     NEXTAUTH_SECRET: "tcUd+DX/gAlVw6tWzGKw6QE2Zi2reXoOuWI2LJw5YGo=",
-  },*/
+  },
 };
 
-const withPWA = require("next-pwa")({
+/*const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disableDevLogs: true,
 });
 
-module.exports = withPWA({ nextConfig });
+module.exports = withPWA({ nextConfig });*/
+module.exports = nextConfig;
